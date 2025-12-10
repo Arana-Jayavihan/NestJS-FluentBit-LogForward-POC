0c7d802caa4831d4cf247fc27babe7edc78eb9b1
Arana-Jayavihan <aranajayavihan@gmail.com>
2025-12-10 14:50:07 +0530

    Implemented Syslog Transport

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nix flake for NestJS FluentBit Log Integration";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = { self, nixpkgs-unstable, ...}:
+
+  let
+    system = "x86_64-linux";
+    pkgs-unstable = import nixpkgs-unstable { inherit system; };
+  in
+
+  {
+    devShells.${system}.default = pkgs-unstable.mkShell {
+      buildInputs = with pkgs-unstable; [
+        nodejs_24
+        fluent-bit
+      ];
+    };
+  };
+}
---
+++ nest-js-fb-log-inter-poc/src/app.module.ts
@@ -0,0 +1,60 @@
+
+import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { destination } from 'pino';
+
+@Module({
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          pipeline: [
+            {
+              target: 'pino-syslog',
+              options: {
+                facility: 23, // local7
+                appname: 'local7'
+              }
+            },
+            {
+              target: 'pino-socket',
+              options: {
+                mode: 'tcp',
+                address: '127.0.0.1',
+                port: 9000
+              }
+            }
+          ],
+          targets: [
+            // Pretty console output for development
+            {
+              target: 'pino-pretty',
+              level: 'debug',
+              options: {
+                colorize: true,
+                singleLine: true,
+                translateTime: 'SYS:standard',
+              },
+            }
+          ],
+        },
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        serializers: {
+          req: (req) => ({
+            id: req.id,
+            method: req.method,
+            url: req.url,
+          }),
+          res: (res) => ({
+            statusCode: res.statusCode,
+          }),
+        },
+      },
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
